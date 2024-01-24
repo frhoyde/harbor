91006c99095956bcdc4a96556872fed8836b815a
omorShahriar <omor.shahriar2000@gmail.com>
2024-01-24 17:22:20 +0600

    okay

--- db/generate-keyfile.sh
+++ db/generate-keyfile.sh
@@ -6,4 +6,4 @@ openssl rand -base64 756 > /opt/keyfile/mongo-keyfile
 # Set the keyfile permissions to 400 and change the owner
 
 chmod 400 /opt/keyfile/mongo-keyfile
-chown 999:999 /opt/keyfile/mongo-keyfile
+chown mongodb:mongodb /opt/keyfile/mongo-keyfile
